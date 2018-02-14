ff45a30a872ae1d847815c438d16abd48ed95918
Peter Seger <segerpeter07@gmail.com>
2018-02-14 14:04:00 -0500

    Moved to different mapbox version

--- frontend/src/js/components/Create_Run.js
+++ frontend/src/js/components/Create_Run.js
@@ -3,10 +3,14 @@ import { Component, Card, CardImg, CardText, CardBody,
     CardTitle, CardLink, CardSubtitle, Button, Form,
     FormGroup, Label, Input, FormText} from 'reactstrap';
 import { Link } from 'react-router';
-import ReactMapGL from 'react-map-gl';
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 import Dimensions from 'react-dimensions';
+var Geocoder = require('../');
 
 
+const Map = ReactMapboxGl({
+  accessToken: "pk.eyJ1IjoicHNlZ2VyIiwiYSI6ImNqZDlsMndiMjYxYWYyd24ycTVvaG1hbHoifQ.LVL_EXnvKNDCKGr-emYKQQ"
+});
 
 class Create extends React.Component {
 
@@ -14,15 +18,17 @@ class Create extends React.Component {
         viewport: {
           width: this.props.containerWidth,
           height: 400,
-          latitude: 51.5074,
-          longitude: -0.1278,
+          latitude: 42.36,
+          longitude: -71.06,
           zoom: 12
         }
       };
 
-    render() {
-        const MapboxAccessToken = 'pk.eyJ1IjoicHNlZ2VyIiwiYSI6ImNqZDlsMndiMjYxYWYyd24ycTVvaG1hbHoifQ.LVL_EXnvKNDCKGr-emYKQQ';
+    temper(value){
+        this.setState({ value: value });
+    }
 
+    render() {
         return (
             <div>
                 <h1>Create a New Run</h1>
@@ -45,12 +51,22 @@ class Create extends React.Component {
                                     <Button color="success" type="submit">Search</Button>
                                 </Form>
                         </CardBody>
-                        <ReactMapGL
-                            mapStyle="mapbox://styles/pseger/cjd9o709fa3ko2rnv0xz2lvnc"
-                            {...this.state.viewport}
-                            mapboxApiAccessToken = {MapboxAccessToken}
-                            onViewportChange={(viewport) => this.setState({viewport})}
-                        />
+                        <Map
+                          style="mapbox://styles/mapbox/streets-v9"
+                          center={[this.state.viewport.longitude, this.state.viewport.latitude]}
+                          zoom={[this.state.viewport.zoom]}
+                          containerStyle={{
+                            height: 400,
+                            width: this.props.containerWidth
+                          }}>
+                            <Layer
+                              type="symbol"
+                              id="marker"
+                              layout={{ "icon-image": "marker-15" }}>
+
+                            </Layer>
+                        </Map>
+
                   </Card>
             </div>
         );
--- frontend/src/js/components/Run_Feed.js
+++ frontend/src/js/components/Run_Feed.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Component, Card, CardImg, CardText, CardBody,
   CardTitle, CardLink, CardSubtitle, Button } from 'reactstrap';
 import { Link } from 'react-router';
-import ReactMapGL from 'react-map-gl';
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 import Dimensions from 'react-dimensions';
 
 var button;
+
+const Map = ReactMapboxGl({
+  accessToken: "pk.eyJ1IjoicHNlZ2VyIiwiYSI6ImNqZDlsMndiMjYxYWYyd24ycTVvaG1hbHoifQ.LVL_EXnvKNDCKGr-emYKQQ"
+});
 
 class Run_Feed extends React.Component {
 
@@ -30,16 +34,25 @@ class Run_Feed extends React.Component {
                   <CardTitle tag="h2"><strong>{this.props.title}</strong></CardTitle>
                   <CardSubtitle>{this.props.city}</CardSubtitle>
                 </CardBody>
-                <ReactMapGL
-                    mapStyle="mapbox://styles/pseger/cjd9o709fa3ko2rnv0xz2lvnc"
-                    {...this.state.viewport}
-                    mapboxApiAccessToken = {MapboxAccessToken}
-                    onViewportChange={(viewport) => this.setState({viewport})}
-                />
+                <Map
+                  style="mapbox://styles/mapbox/streets-v9"
+                  center={[this.props.lng, this.props.lat]}
+                  zoom={[12]}
+                  containerStyle={{
+                    height: 400,
+                    width: this.props.containerWidth
+                  }}>
+                    <Layer
+                      type="symbol"
+                      id="marker"
+
+                      layout={{ "icon-image": "marker-15" }}>
+                    </Layer>
+                </Map>
                 <CardBody>
                   <CardText>{this.props.description}</CardText>
                   <CardText>ID: {this.props.pathid}</CardText>
-                  <Link href={`paths/${this.props.pathid}`}><Button color="primary">Learn More</Button></Link>;
+                  <Link href={`paths/${this.props.pathid}`}><Button color="primary">Learn More</Button></Link>
                 </CardBody>
               </Card>
             </div>
