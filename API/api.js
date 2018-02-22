082c941bdc9e837194f166ed1d4929eebe7b4af1
Nina Tchirkova <nina.tchirkova@students.olin.edu>
2018-02-21 22:05:27 -0500

    Changes to routes

--- API/api.js
+++ API/api.js
@@ -7,15 +7,23 @@ var mysql = require('mysql');
 
 var router = express.Router();
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "strolle_app",
+  password: "walk",
+  database: "strolle_db"
+});
+
 router.route('/paths')
     // Create a new paths.
     .post(function(req, res) {
-        t = req.title;
-        loc = req.loc_name;
-        d = req.description;
-        lt = req.lat;
-        ln = req.long;
-        var path_insert = `INSERT INTO paths (title, location_name, description, latitude, longitude) VALUES ("${t}", "${loc}", "${d}", ${lt}, ${ln})`;
+        console.log(req.body.key, req.body.name);
+        // var t = req.title;
+        // var loc = req.loc_name;
+        // var d = req.description;
+        // var lt = req.lat;
+        // var ln = req.long;
+        // var path_insert = `INSERT INTO paths (title, location_name, description, latitude, longitude) VALUES ("${t}", "${loc}", "${d}", ${lt}, ${ln})`;
         con.connect(function(err) {
           if (err) throw err;
           con.query(path_insert, function (err, result) {
@@ -23,7 +31,8 @@ router.route('/paths')
             console.log("1 record inserted");
           });
         });
-        console.log("Successfully POSTing")
+        // console.log("Successfully POSTing")
+        res.send('hi');
     })
 
 router.route('/paths/:pathID')
@@ -37,7 +46,7 @@ router.route('/paths/:pathID')
           con.query(path_get, function (err, result, fields) {
             if (err) throw err;
             path = result[0];
-            res.json(path)
+            res.json(path);
           });
         });
         console.log("Successfully GETing")
@@ -74,27 +83,6 @@ var con = mysql.createConnection({
   database: "strolle_db"
 });
 
-function setup_db() {
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query("CREATE DATABASE strolle_db", function (err, result) {
-      if (err) throw err;
-      console.log("Database created");
-    });
-  });
-  con.end()
-}
-
-function setup_paths_table() {
-  con.connect(function(err) {
-    var sql = "CREATE TABLE paths (ID int NOT NULL AUTO_INCREMENT, title VARCHAR(255),location_name VARCHAR(255),description TEXT,latitude FLOAT(50,25),longitude FLOAT(50,25), primary key (ID))";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
-    });
-  });
-}
 
 function create_path(title, loc_name, description, lat, long) {
   t = title;
