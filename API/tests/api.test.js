560f54cc71c5e1cd366ed88431dfb05897caa666
Nina Tchirkova <nina.tchirkova@students.olin.edu>
2018-02-26 21:33:28 -0500

    first test written

--- API/tests/api.test.js
+++ API/tests/api.test.js
@@ -1,22 +1,42 @@
 var express = require('express');
 var path = require('path');
-var assert = require('assert');
-
 var mysql = require('mysql');
-
-
+const assert = require('assert');
+const chai = require("chai");
+const expect = chai.expect;
 var router = express.Router();
+const request = require('superagent');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "strolle_app",
-  password: "walk",
-  database: "strolle_test"
-});
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "strolle_app",
+//   password: "walk",
+//   database: "strolle_test"
+// });
+var example = {
+    title: "Walk in the park",
+    description : "This was fun",
+    location_name : "Boston",
+    lat : 25.3,
+    lng : 23.99
+};
 
 describe("smoke test", function() {
   it("checks equality", function() {
-    assert.equal(true, true);
+    expect(true).to.be.true;
+  });
+});
+
+describe("post test", function() {
+  it("checks that walk gets added to db", function() {
+    request
+        .post('/api/paths')
+        .send(example)
+        .then(function(res) {
+            expect(res).to.equal("1 record inserted");
+        }).catch(function () {
+            console.log("Reached the catch");
+        });
   });
 });
 
