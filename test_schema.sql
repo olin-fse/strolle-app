51da9a177b3849065df6e332a173f12b0b21116b
Peter Seger <segerpeter07@gmail.com>
2018-03-08 21:36:58 -0500

    Cleaned directory

--- test_schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE strolle_test;
-USE strolle_test;
-CREATE TABLE paths (ID int NOT NULL AUTO_INCREMENT,
-  title TEXT,location_name TEXT,
-  description TEXT,
-  latitude FLOAT(50,25),
-  longitude FLOAT(50,25),
-  primary key (ID)
-);
